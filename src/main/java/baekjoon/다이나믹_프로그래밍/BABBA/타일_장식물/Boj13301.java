1f34e3af8dc8050746b3f20626cc8fe6e79ee56a
sujin cho <sujinemilycho@gmail.com>
2021-11-18 22:05:45 +0900

    2021/11/18 백준 다이나믹 프로그래밍 3301 타일 장식물

---
+++ src/main/java/baekjoon/다이나믹_프로그래밍/BABBA/타일_장식물/Boj13301.java
@@ -0,0 +1,20 @@
+package baekjoon.다이나믹_프로그래밍.BABBA.타일_장식물;
+
+import java.util.Scanner;
+
+public class Boj13301 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long[] tiles = new long[N + 2];
+        tiles[1] = 1;
+        tiles[2] = 1;
+
+        for (int i = 3; i <= N; i++) {
+            tiles[i] = tiles[i - 1] + tiles[i - 2];
+        }
+
+        long area = 2 * (tiles[N] + tiles[N] + tiles[N - 1]);
+        System.out.println(area);
+    }
+}
