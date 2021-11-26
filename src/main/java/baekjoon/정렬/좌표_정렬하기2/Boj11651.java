70418d68a0394ec8022fe525c49e80c186879129
sujin cho <sujinemilycho@gmail.com>
2021-11-26 17:08:37 +0900

    2021/11/26 백준 정렬 11651 좌표 정렬하기

---
+++ src/main/java/baekjoon/정렬/좌표_정렬하기2/Boj11651.java
@@ -0,0 +1,32 @@
+package baekjoon.정렬.좌표_정렬하기2;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Boj11651 {
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        List<int[]> coordinates = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            int[] point = {sc.nextInt(), sc.nextInt()};
+            coordinates.add(point);
+        }
+
+        coordinates.sort(Comparator.<int[], Integer>comparing(p -> p[1]).thenComparing(p -> p[0]));
+
+        StringBuilder sb = new StringBuilder();
+        for (int[] points : coordinates) {
+            sb.append(points[0]);
+            sb.append(" ");
+            sb.append(points[1]);
+            sb.append(LINE_SEPARATOR);
+        }
+        System.out.println(sb.toString());
+    }
+}
