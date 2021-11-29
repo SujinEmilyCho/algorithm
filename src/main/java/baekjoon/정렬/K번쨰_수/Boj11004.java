f8ea259b8c9cb069583e60ca6a17cda86417561e
sujin cho <sujinemilycho@gmail.com>
2021-11-29 12:48:04 +0900

    2021/11/29 백준 정렬 11004 K번째 수

---
+++ src/main/java/baekjoon/정렬/K번쨰_수/Boj11004.java
@@ -0,0 +1,27 @@
+package baekjoon.정렬.K번쨰_수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj11004 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] numbers = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+
+        System.out.println(numbers[K - 1]);
+    }
+}
